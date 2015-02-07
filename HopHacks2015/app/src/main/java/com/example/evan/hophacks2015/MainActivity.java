9f0edad65da791df740d761452bb436214c898ec
Sarah Hendricks <slynnhendricks@gmail.com>
2015-02-07 01:10:40 -0500

    Prepare Main Activity
    Added comments

--- HopHacks2015/app/src/main/java/com/example/evan/hophacks2015/MainActivity.java
+++ HopHacks2015/app/src/main/java/com/example/evan/hophacks2015/MainActivity.java
@@ -17,6 +17,9 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //program in here
+
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
